6b27a1666a714a34f3e4bd655aadeed6faf6a0a9
Dhouib25 <mohamed.dhouib@insat.ucar.tn>
2021-12-21 19:18:25 +0100

    Initial commit

---
+++ lib/Interfaces/Login.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:iac_project/Widgets/login_button_colored.dart';
+import 'package:iac_project/Widgets/login_button_normal.dart';
+
+class Login extends StatelessWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body:
+        Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: const [
+            SizedBox(
+              width: double.infinity,
+              height: 300,
+              child: Image(
+                  image: AssetImage("assets/Images/img1.png"),
+              ),
+            ),
+            LoginButtonColored(name: "Connect via Google", icon: Icons.ac_unit_rounded, c: Color(0xffdd4b39), role: Login),
+            LoginButtonColored(name: "Connect via Facebook", icon: Icons.ac_unit_rounded, c: Color(0xff3B5998), role: Login),
+            LoginButtonColored(name: "Connect via Apple", icon: Icons.kayaking, c: Color(0xff000000), role: Login),
+            LoginButtonColored(name: "Connect via Email", icon: Icons.mail, c: Colors.blueGrey, role: Login),
+            LoginButtonNormal(name: "Create an account", role: Login),
+            Padding(
+              padding: EdgeInsets.only(top: 30),
+              child: SizedBox(
+                width: 327,
+                height: 45,
+                child: Text(
+                  "By signing up, you are agreeing to our Terms & Conditions",
+                  style: TextStyle(
+                    fontSize: 13
+                  ) ,),
+              ),
+            )
+
+          ],
+
+        ),
+
+    );
+
+
+  }
+
+}
---
+++ lib/Widgets/login_button_colored.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class LoginButtonColored extends StatelessWidget {
+  final IconData ?icon ;
+  final String ?name ;
+  final Color ?c ;
+  final Type role;
+  const LoginButtonColored({Key? key,required this.name,required this.icon,required this.c,required this.role}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Padding(
+      padding: const EdgeInsets.only(right:32,top: 10,left:32,bottom: 10),
+      child: TextButton(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(c!),
+            fixedSize: MaterialStateProperty.all(Size(322, 62))
+          ),
+          onPressed: () { role; },
+        child: Row(
+          mainAxisAlignment : MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: Icon(
+                icon,
+                color: Colors.white,
+                size: 30,
+              ),
+            ),
+            Text(
+              name!,
+              style: const TextStyle(
+                color:Colors.white,
+                fontSize: 17
+              ),
+
+            )
+
+          ]
+        )
+
+      ),
+    );
+  }
+
+}
