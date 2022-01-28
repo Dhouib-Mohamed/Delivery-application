e293f0138ad861e7fd5903a3bf0372a00dc58d9b
Dhouib25 <mohamed.dhouib@insat.ucar.tn>
2022-01-28 20:41:54 +0100

    Update

---
+++ lib/Interfaces/feed.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Feed extends StatefulWidget {
+  const Feed({Key? key}) : super(key: key);
+
+  @override
+  State<Feed> createState() => _Feed();
+}
+
+class _Feed extends State<Feed> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
