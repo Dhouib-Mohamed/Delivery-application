61e34c8076fe44dd18006ac1bfcc0d6a50db1380
Dhouib25 <mohamed.dhouib@insat.ucar.tn>
2022-03-22 15:34:00 +0100

    Update

--- lib/Interfaces/profile.dart
+++ lib/Interfaces/profile.dart
@@ -23,22 +23,18 @@ class _ProfileState extends State<Profile> {
         ),
       ),
       bottomNavigationBar: const BotBar(i: 4),
-      body: Padding(
-        padding: const EdgeInsets.only(top:50),
-        child: SizedBox(
-          height: MediaQuery.of(context).size.height*0.6,
-          child: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left:20,bottom: 15),
-                  child: Row(
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left:15,bottom: 80,right: 15,top: 80),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       const Text(
-                          "NAME : ",
+                          "Name : ",
                           style: TextStyle(
                             fontStyle: FontStyle.italic,
                             color: Color(0xffbd2005),
@@ -49,70 +45,83 @@ class _ProfileState extends State<Profile> {
                           "${globals.user!.name[0].toUpperCase()}${globals.user!.name.substring(1)}",
                           style: const TextStyle(
                             color: Colors.blueGrey,
-                            fontSize: 21,
-                            fontWeight: FontWeight.w400,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w300,
                           ),
                         ),
                     ],
                   ),
-                ),
                   Padding(
-                    padding: const EdgeInsets.only(left:20,bottom: 15),
-                    child: Row(
-                      children: [
-                        const Text(
-                          "EMAIL : ",
-                          style: TextStyle(
-                            fontStyle: FontStyle.italic,
-                            color: Color(0xffbd2005),
-                            fontSize: 19,
-                          ),
-                        ),
-                        Text(
-                          globals.user!.email,
-                          style: const TextStyle(
-                            color: Colors.blueGrey,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                      ],
+                    padding: const EdgeInsets.only(top: 15,bottom: 15),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 3,
+                      color: const Color(0xD7DAE0E3),
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(left:20,bottom:40),
-                    child: Row(
-                      children: [
-                        const Text(
-                          "PHONE NUMBER : ",
-                          style: TextStyle(
-                            fontStyle: FontStyle.italic,
-                            color: Color(0xffbd2005),
-                            fontSize: 19,
-                          ),
-                        ),
-                        Text(
-                          globals.user!.phone!,
-                          style: const TextStyle(
-                            color: Colors.blueGrey,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                      ],
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Email : ",
+                    style: TextStyle(
+                      fontStyle: FontStyle.italic,
+                      color: Color(0xffbd2005),
+                      fontSize: 20,
                     ),
                   ),
-                  ProfileButton(
-                    name: "Change Password",width: MediaQuery.of(context).size.width, role: ()=>Navigator.pushNamed(context, "/forgot_password"), icon: const Icon(Icons.password,color: Colors.black,)),
-                  ProfileButton(
-                    name: "My Addresses   ",width: MediaQuery.of(context).size.width,role: ()=>Navigator.pushNamed(context, "/address"), icon: const Icon(Icons.location_on,color: Colors.black)),
-                  ProfileButton(
-                    name: "Settings       ",width: MediaQuery.of(context).size.width, role: ()=>Navigator.pushNamed(context, "/settings"), icon: const Icon(Icons.settings_sharp ,color: Colors.black)),
-                  ProfileButton(
-                    name: "Help & FAQ     ",width: MediaQuery.of(context).size.width, role: ()=>Navigator.pushNamed(context, "/help") , icon: const Icon(Icons.help_rounded,color: Colors.black)),
-              ],
+                  Text(
+                    globals.user!.email,
+                    style: const TextStyle(
+                      color: Colors.blueGrey,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                ],
+              ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15,bottom: 15),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 3,
+                      color: const Color(0xD7DAE0E3),
+                    ),
+                  ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Phone Number : ",
+                    style: TextStyle(
+                      fontStyle: FontStyle.italic,
+                      color: Color(0xffbd2005),
+                      fontSize: 20,
+                    ),
+                  ),
+                  Text(
+                    globals.user!.phone!,
+                    style: const TextStyle(
+                      color: Colors.blueGrey,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                ],
+              ),
+
+                ],
+              ),
             ),
-          ),
+              ProfileButton(
+                name: "Change Password",width: MediaQuery.of(context).size.width, role: ()=>Navigator.pushNamed(context, "/forgot_password"), icon: const Icon(Icons.password,color: Colors.black,)),
+              ProfileButton(
+                name: "My Addresses   ",width: MediaQuery.of(context).size.width,role: ()=>Navigator.pushNamed(context, "/address"), icon: const Icon(Icons.location_on,color: Colors.black)),
+              ProfileButton(
+                name: "Settings       ",width: MediaQuery.of(context).size.width, role: ()=>Navigator.pushNamed(context, "/settings"), icon: const Icon(Icons.settings_sharp ,color: Colors.black)),
+              ProfileButton(
+                name: "Help & FAQ     ",width: MediaQuery.of(context).size.width, role: ()=>Navigator.pushNamed(context, "/help") , icon: const Icon(Icons.help_rounded,color: Colors.black)),
+          ],
         ),
       ),
     );
